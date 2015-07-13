dd60e6dbb94d0acbf99a17752a3742fcfeb3d1b1
Darko Aleksovski <darko.aleksovski@ijs.si>
2015-07-13 14:17:11 +0200

    Commit requirements and setup files.

---
+++ setup.py
@@ -0,0 +1,16 @@
+import os
+from setuptools import setup
+
+setup(
+    name='cf_datamining',
+    version='0.1',
+    include_package_data=True,
+    license='MIT License',
+    description='',
+    
+    install_requires=[
+          'scikit-learn>=0.14.1',
+    ],
+    author='',
+    author_email='',
+)
