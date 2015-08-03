28fa848974e505be3e3e20d25e3ee9dbe5df86cc
Darko Aleksovski <darko.aleksovski@ijs.si>
2015-08-03 16:12:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version='0.1',
     include_package_data=True,
     license='MIT License',
-    description='',
+    description='Package providing basic data mining widgets for ClowdFlows 2.0',
     
     install_requires=[
           'scikit-learn>=0.14.1',
