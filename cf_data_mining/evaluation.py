5f751e19e9a21fb916e28ca2334ebeb35f372b75
Darko Aleksovski <darko.aleksovski@ijs.si>
2016-01-13 14:11:36 +0100

    Added classification statistics widget

--- cf_data_mining/evaluation.py
+++ cf_data_mining/evaluation.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 __author__ = 'daleksovski'
 
+from sklearn import metrics
+
 def build_classifier(classifier, data):
     '''Builds a classifier
 
@@ -61,3 +63,59 @@ def mse(data):
 
 
     return result_mse
+
+def calculate_classification_statistics(dataset):
+
+    # Old format:
+    # 'Expected true and predicted labels for each fold, but failed.' +
+    # 'If you wish to provide labels for each fold separately it should look like: ' +
+    # '[[y_true_1, y_predicted_1], [y_true_2, y_predicted_2], ...]')
+
+    labels = [[],[]]
+    for i in range(0,len(dataset.target)):
+        labels[0].append(dataset['target'][i])
+        labels[1].append(dataset['targetPredicted'][i])
+
+    # Check if we have true and predicted labels for each fold
+    if labels and type(labels[0][0]) == list:
+        try:
+            # Flatten
+            y_true, y_pred = [], []
+            for fold_labels in labels:
+                y_true.extend(fold_labels[0])
+                y_pred.extend(fold_labels[1])
+            labels = [y_true, y_pred]
+        except:
+            raise Exception('Expected true and predicted labels for each fold, but failed.' +
+                            'If you wish to provide labels for each fold separately it should look like: ' +
+                            '[[y_true_1, y_predicted_1], [y_true_2, y_predicted_2], ...]')
+    if len(labels) != 2:
+        raise Exception('Wrong input structure, this widget accepts labels in the form: [y_true, y_pred]')
+
+    y_true, y_pred = labels
+
+    classes = set()
+    classes.update(y_true + y_pred)
+    classes = sorted(list(classes))
+
+    # Assign integers to classes
+    class_to_int = {}
+    for i, cls_label in enumerate(classes):
+        class_to_int[cls_label] = i
+
+    y_true = [class_to_int[lbl] for lbl in y_true]
+    y_pred = [class_to_int[lbl] for lbl in y_pred]
+
+    accuracy = metrics.accuracy_score(y_true, y_pred)
+    precision = metrics.precision_score(y_true, y_pred)
+    recall = metrics.recall_score(y_true, y_pred)
+    f1 = metrics.f1_score(y_true, y_pred)
+    confusion_matrix = metrics.confusion_matrix(y_true, y_pred)
+
+    # AUC is defined only for binary classes
+    if len(classes) == 2:
+        auc = metrics.auc_score(y_true, y_pred)
+    else:
+        auc = 'undefined for multiple classes'
+
+    return accuracy, precision, recall, f1, auc, confusion_matrix
