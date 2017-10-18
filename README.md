04abee18c0f67773c9c44fb239290233e799e81b
Anze Vavpetic <anze.vavpetic@ijs.si>
2017-10-18 10:53:50 +0200

    Adds license file and setup.py fix

---
+++ README.md
@@ -0,0 +1,2 @@
+The ClowdFlows Data Mining Package.
+Contains data mining widgets based on scikit-learn. The package is intended to be used along with ClowdFlows 2.0.
--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import os
 
-setup(
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+]
+
+dist = setup(
     name='cf_data_mining',
     version='0.1',
-    include_package_data=True,
     license='MIT License',
     description='Package providing basic data mining widgets (based on scikit-learn) for ClowdFlows 2.0',
-    
+    url='https://github.com/xflows/cf_data_mining',
+    classifiers=CLASSIFIERS,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
     install_requires=[
-          'scikit-learn>=0.14.1',
-    ],
-    author='',
-    author_email='',
+        'cf_core',
+        'scipy',
+        'numpy',
+        'scikit-learn==0.15.2',
+        'pydot==1.0.2'
+    ]
 )
